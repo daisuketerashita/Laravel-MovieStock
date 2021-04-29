28629ad4359f28ee177c9e16376b774d46b002ec
寺下大典 <terashitadaisuke@terashitaMacBookPro.local>
2021-04-29 14:24:59 +0900

    ログインページの調整

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,7 +45,7 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('パスワードを保存する') }}
+                                        {{ __('ログイン状態を保持する') }}
                                     </label>
                                 </div>
                             </div>
